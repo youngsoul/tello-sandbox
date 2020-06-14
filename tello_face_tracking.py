8170cdcfccaa3dced5164b3097a67fc57eb29ba6
patrickryan <theyoungsoul@gmail.com>
2020-06-14 10:10:47 -0500

    minor - moved global line in function to first line, added image of tello

--- tello_face_tracking.py
+++ tello_face_tracking.py
@@ -173,11 +173,11 @@ def show_video(exit_event, pipe_conn):
 
 
 def video_recorder(pipe_conn, save_video, height=300, width=400):
+    global video_writer
     # create a VideoWrite object, recoring to ./video.avi
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
-    global video_writer
     if video_writer is None and save_video == True:
         video_file = f"video_{datetime.now().strftime('%d-%m-%Y_%I-%M-%S_%p')}.mp4"
         video_writer = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc(*'MP4V'), 30, (width, height))
